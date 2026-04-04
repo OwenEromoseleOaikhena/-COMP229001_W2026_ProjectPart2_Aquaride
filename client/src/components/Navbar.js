806325b4f869073b801ee07833f5f5505aab063f
OwenEromoseleOaikhen <ooaikhen@my.centennialcollege.ca>
2026-04-04 18:53:06 -0400

    Fix Navbar - show only Login when logged out per rubric

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -36,19 +36,22 @@ const Navbar = () => {
 
           <ul className="navbar-nav ms-auto">
             {user ? (
-              // Show when logged in
+              // Logged in: show Dashboard + Logout only
               <>
-                <li className="nav-item"><Link className="nav-link" to="/dashboard">Dashboard</Link></li>
                 <li className="nav-item">
-                  <button className="btn btn-outline-light btn-sm ms-2" onClick={handleLogout}>Logout</button>
+                  <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                </li>
+                <li className="nav-item">
+                  <button className="btn btn-outline-light btn-sm ms-2" onClick={handleLogout}>
+                    Logout
+                  </button>
                 </li>
               </>
             ) : (
-              // Show when logged out
-              <>
-                <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
-                <li className="nav-item"><Link className="btn btn-light btn-sm ms-2" to="/register">Sign Up</Link></li>
-              </>
+              // Logged out: show Login only (rubric requirement)
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+              </li>
             )}
           </ul>
         </div>
